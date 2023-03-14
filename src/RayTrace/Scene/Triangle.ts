32f1eca83f0ae2ac0ab2598bec7311f357e28bbd
hannahellis4242 <hannahellis4242@gmail.com>
2023-03-14 23:10:17 +0000

    Triangle and spec

--- src/RayTrace/Scene/Triangle.ts
+++ src/RayTrace/Scene/Triangle.ts
@@ -1,5 +1,11 @@
-import matrix3, { det } from "../../lin-alg/matrix3";
-import Vector, { cross, subtract, unit,scale } from "../../mesh/Vector";
+import Vector, {
+  cross,
+  subtract,
+  unit,
+  scale,
+  dot,
+  add,
+} from "../../mesh/Vector";
 import Ray from "./Ray";
 
 export interface IntersectionParams {
@@ -11,7 +17,7 @@ export interface IntersectionParams {
 export default class Triangle {
   private readonly p01: Vector;
   private readonly p02: Vector;
-  private readonly normal:Vector;
+  private readonly normal: Vector;
   public readonly unitNormal: Vector;
   constructor(public readonly points: [Vector, Vector, Vector]) {
     this.p01 = subtract(this.points[1], this.points[0]);
@@ -19,37 +25,33 @@ export default class Triangle {
     this.normal = cross(this.p01, this.p02);
     this.unitNormal = unit(this.normal);
   }
-  intersects(l: Ray): IntersectionParams|undefined {
-    const lab = scale(-1,l.direction());
-    const det = dot(lab,this.normal);
-    if(det === 0)
-    {
+  intersects(l: Ray): IntersectionParams | undefined {
+    const lab = scale(-1)(l.direction());
+    const det = dot(lab, this.normal);
+    if (det === 0) {
       return undefined;
     }
-    const b = subtract(l.source,this.points[0]);
-    const u = dot(cross(this.p02,lab),b)/det;
-    if( u < 0 || u > 1)
-    {
+    const b = subtract(l.source, this.points[0]);
+    const u = dot(cross(this.p02, lab), b) / det;
+    if (u < 0 || u > 1) {
       return undefined;
     }
-    const v = dot(cross(lab,this.p02),b)/det;
-    if( v < 0 || v > 1)
-    {
+    const v = dot(cross(lab, this.p02), b) / det;
+    if (v < 0 || v > 1) {
       return undefined;
     }
-    if(u+v>1)
-    {
+    if (u + v > 1) {
       return undefined;
     }
-    const t = dot(this.normal,b)/det;
-    return {t,u,v};
+    const t = dot(this.normal, b) / det;
+    return { t, u, v };
   }
-  point(u:number,v:number):Vector|undefined{
-    if(u<0 || v<0 || u+v>1){
+  point(u: number, v: number): Vector | undefined {
+    if (u < 0 || v < 0 || u + v > 1) {
       return undefined;
     }
-    const a = scale(u,this.p01);
-    const b = scale(v,this.p02);
-    return add(this.points[0],add(a,b));
+    const a = scale(u)(this.p01);
+    const b = scale(v)(this.p02);
+    return add(this.points[0], add(a, b));
   }
 }
