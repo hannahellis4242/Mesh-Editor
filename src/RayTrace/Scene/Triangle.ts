d0edfd4cc0de1597fedc8b7fc7e4509fb7e79f03
hannahellis4242 <hannahellis4242@gmail.com>
2023-03-14 09:20:25 +0000

    started working on determining if a ray intersects a triangle

--- src/RayTrace/Scene/Triangle.ts
+++ src/RayTrace/Scene/Triangle.ts
@@ -1,15 +1,39 @@
+import matrix3, { det } from "../../lin-alg/matrix3";
 import Vector, { cross, subtract, unit } from "../../mesh/Vector";
 import Ray from "./Ray";
 
+export interface IntersectionParams {
+  t: number;
+  u: number;
+  v: number;
+}
+
 export default class Triangle {
+  private readonly p01: Vector;
+  private readonly p02: Vector;
   public readonly unitNormal: Vector;
   constructor(public readonly points: [Vector, Vector, Vector]) {
-    this.unitNormal = unit(
-      cross(
-        subtract(this.points[1], this.points[0]),
-        subtract(this.points[2], this.points[0])
-      )
-    );
+    this.p01 = subtract(this.points[1], this.points[0]);
+    this.p02 = subtract(this.points[2], this.points[0]);
+    this.unitNormal = unit(cross(this.p01, this.p02));
   }
-  intersects(l: Ray) {}
+  intersects(l: Ray): boolean {
+    const lab = l.direction();
+    const m: matrix3 = [
+      -lab.x,
+      this.p01.x,
+      this.p02.x,
+      -lab.y,
+      this.p01.y,
+      this.p02.y,
+      -lab.z,
+      this.p01.x,
+      this.p02.z,
+    ];
+    if (det(m) === 0) {
+      return false;
+    }
+    //TODO do more here
+    return false;
+  }
 }
