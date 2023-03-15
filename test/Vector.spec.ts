923b7ff757d184e51c241743edff23152e9a1a24
hannahellis4242 <hannahellis4242@gmail.com>
2023-03-15 00:14:38 +0000

    adding more tests

--- test/Vector.spec.ts
+++ test/Vector.spec.ts
@@ -1,4 +1,10 @@
-import Vector, { add, negate, zeroVec } from "../src/mesh/Vector";
+import Vector, {
+  add,
+  negate,
+  scale,
+  subtract,
+  zeroVec,
+} from "../src/mesh/Vector";
 
 describe("Vector", () => {
   describe("when zero vector", () => {
@@ -26,6 +32,26 @@ describe("Vector", () => {
       expect(x).toBe(-5);
       expect(y).toBe(1);
       expect(z).toBe(-2);
+    });
+  });
+  describe("subtract", () => {
+    const a = { x: 5, y: -1, z: 2 };
+    const b = { x: 2, y: -2, z: 4 };
+    it("should give (3,1,-2)", () => {
+      const { x, y, z } = subtract(a, b);
+      expect(x).toBe(3);
+      expect(y).toBe(1);
+      expect(z).toBe(-2);
+    });
+  });
+  describe("scale", () => {
+    const s = scale(5);
+    const a = { x: 5, y: -1, z: 2 };
+    it("should give (25,-5,10)", () => {
+      const { x, y, z } = s(a);
+      expect(x).toBe(25);
+      expect(y).toBe(-5);
+      expect(z).toBe(10);
     });
   });
 });
