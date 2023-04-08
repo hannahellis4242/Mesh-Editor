e5d8b997ad8e0355f03b43aa56939bea9c7c697c
hannahellis4242 <hannahellis4242@gmail.com>
2023-04-08 23:06:28 +0100

    Forgot to remove this

--- src/utils/resizeCanvasToDisplaySize.ts
+++
@@ -1,6 +0,0 @@
-const resizeCanvasToDisplaySize = (canvas: HTMLCanvasElement) => {
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
-};
-
-export default resizeCanvasToDisplaySize;
