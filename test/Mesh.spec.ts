4ac145aacb4b873caa71f6c0062012aedbb8fdc6
hannahellis4242 <hannahellis4242@gmail.com>
2023-04-09 08:14:34 +0100

    adding first remove vertex test

--- test/Mesh.spec.ts
+++ test/Mesh.spec.ts
@@ -15,18 +15,27 @@ const addVertex = (parent: Mesh, vertex: Vector) => {
   return found ? parent : build(parent.vertices.concat(vertex));
 };
 
+const removeVertex = (parent: Mesh, index: number) => {
+  if (index < 0) {
+    return parent;
+  }
+  if (index >= parent.vertices.length) {
+    return parent;
+  }
+  return build(parent.vertices.filter((_, i) => i == index));
+};
+
 describe("Mesh", () => {
   describe("unit mesh", () => {
-    const unitMesh = unit();
-    it("should have no vertices", () =>
-      expect(unitMesh.vertices).toHaveLength(0));
+    const init = unit();
+    it("should have no vertices", () => expect(init.vertices).toHaveLength(0));
   });
   describe("when adding a vertex to a unit mesh", () => {
-    const unitMesh = unit();
-    const child = addVertex(unitMesh, vec(1, 2, 3));
-    it("should have one vertex", () => expect(child.vertices).toHaveLength(1));
+    const init = unit();
+    const mesh = addVertex(init, vec(1, 2, 3));
+    it("should have one vertex", () => expect(mesh.vertices).toHaveLength(1));
     it("and it should have the same value as the vertex added", () => {
-      const [vertex] = child.vertices;
+      const [vertex] = mesh.vertices;
       const { x, y, z } = vertex;
       expect(x).toBe(1);
       expect(y).toBe(2);
@@ -35,11 +44,10 @@ describe("Mesh", () => {
   });
   describe("adding two different vertices to a unit mesh", () => {
     const init = addVertex(unit(), vec(1, 2, 3));
-    const child = addVertex(init, vec(2, 4, 9));
-    it("should have two vertices", () =>
-      expect(child.vertices).toHaveLength(2));
+    const mesh = addVertex(init, vec(2, 4, 9));
+    it("should have two vertices", () => expect(mesh.vertices).toHaveLength(2));
     it("and it should contain the same value as the vertex added", () => {
-      const { x, y, z } = child.vertices.at(-1)!;
+      const { x, y, z } = mesh.vertices.at(-1)!;
       expect(x).toBe(2);
       expect(y).toBe(4);
       expect(z).toBe(9);
@@ -47,7 +55,13 @@ describe("Mesh", () => {
   });
   describe("adding a second vertex that is the same as the first", () => {
     const init = addVertex(unit(), vec(1, 2, 3));
-    const child2 = addVertex(init, vec(1, 2, 3));
-    it("should have one vertex", () => expect(child2.vertices).toHaveLength(1));
+    const mesh = addVertex(init, vec(1, 2, 3));
+    it("should have one vertex", () => expect(mesh.vertices).toHaveLength(1));
+  });
+  describe("removing an existing vertex from the mesh", () => {
+    const init = addVertex(addVertex(unit(), vec(1, 2, 3)), vec(2, 3, 4));
+    const mesh = removeVertex(init, 1);
+    it("should now have only one vertex", () =>
+      expect(mesh.vertices).toHaveLength(1));
   });
 });
