a9519420c397eeece652ef599a6c563ccf5050d2
hannahellis4242 <hannahellis4242@gmail.com>
2023-04-16 10:25:40 +0100

    extract replaceSurface

--- src/Mesh2/Mesh.ts
+++ src/Mesh2/Mesh.ts
@@ -1,8 +1,6 @@
-import Triangle, { Indices, equalTriangles, triangle } from "./Triangle";
+import Triangle from "./Triangle";
 import Vector from "./Vector";
-import addSurface from "./addSurface";
 import buildMesh from "./buildMesh";
-import surfaceCanBeAdded from "./surfaceCanBeAdded";
 
 export default interface Mesh {
   readonly vertices: Vector[];
@@ -11,26 +9,6 @@ export default interface Mesh {
 
 //surface crud
 
-//update
-export interface ReplaceSurface {
-  index: number;
-  value: Indices;
-}
-export const replaceSurface = (
-  mesh: Mesh,
-  { index, value }: ReplaceSurface
-): Mesh => {
-  const found = mesh.surfaces.at(index);
-  if (!found) {
-    return mesh;
-  }
-  return surfaceCanBeAdded(value)(mesh)
-    ? buildMesh(
-        mesh.vertices,
-        mesh.surfaces.map((v, i) => (i === index ? triangle(value) : v))
-      )
-    : mesh;
-};
 //delete
 export const removeSurface = (mesh: Mesh, index: number): Mesh => {
   const found = mesh.surfaces.at(index);
