7763ebd820ef59c87ad88334b1a57b121febb5a6
hannahellis4242 <hannahellis4242@gmail.com>
2023-04-16 10:37:28 +0100

    forgot to remove the function from mesh

--- src/Mesh2/Mesh.ts
+++ src/Mesh2/Mesh.ts
@@ -6,17 +6,3 @@ export default interface Mesh {
   readonly vertices: Vector[];
   readonly surfaces: Triangle[];
 }
-
-//surface crud
-
-//delete
-export const removeSurface = (mesh: Mesh, index: number): Mesh => {
-  const found = mesh.surfaces.at(index);
-  if (!found) {
-    return mesh;
-  }
-  return buildMesh(
-    mesh.vertices,
-    mesh.surfaces.filter((_, i) => i !== index)
-  );
-};
